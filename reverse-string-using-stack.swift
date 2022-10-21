e29081554b93a116f7080617ddd658fa7c02ba37
Fantech Developer <akbarstudyinfo786@gmail.com>
2022-10-21 16:25:14 +0500

    Create reverse-string-using-stack.swift

---
+++ reverse-string-using-stack.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+class  Stack {
+var list: [String] = []
+
+func push(_ item: String) {
+    self.list.append(item)
+}
+
+func pop() -> String {
+    return self.list.removeLast()
+}
+
+func isEmpty() -> Bool {
+    return self.list == []
+}
+
+func length() -> Int {
+    return self.list.count
+}
+}
+
+func reverse(string: String) -> String {
+    let s: Stack = Stack()
+    var reverse: String = ""
+
+    string.forEach {
+        s.push(String($0))
+    } 
+    if s.isEmpty() { return "" }
+    let SIZE: Int = s.length()
+    var i: Int = 0
+
+    while i < SIZE {
+        reverse += s.pop()
+        i += 1
+    }
+
+    return reverse
+}
+
+print(reverse(string: "Hello"))
